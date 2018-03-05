b6612a03396dde0ff149e1baf276483d80740814
Artem Pilipenko <ArtemPilipenko5@gmail.com>
2018-03-05 21:01:23 +0400

    Добавлены 2 файла для работы с гаражом

---
+++ Game/Project/js/garage.js
@@ -0,0 +1,18 @@
+﻿//сам канвас (логический)
+var field=document.getElementById('canvas');
+//позволяет рисовать по канвасу
+var context = field.getContext('2d');
+
+var image = new Image();
+image.src = '../images/maps/map1.png';
+ 
+image.onload = function () {
+    context.drawImage(image, 0, 0);
+};
+ 
+var imageCar = new Image();
+imageCar.src = '../images/cars/1/car.png';
+
+imageCar.onload = function () {
+    context.drawImage(imageCar, 0, 130);
+};
